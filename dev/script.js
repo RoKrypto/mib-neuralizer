4302f78a454226c28583e4b248526b0405bf8cc5
Rodrigo Mendoza <rk_2443@hotmail.com>
2024-11-27 00:04:27 -0300

    File includes a call to Service Worker

--- dev/script.js
+++ dev/script.js
@@ -38,3 +38,9 @@ flashBtn.addEventListener('click', async () => {
 sound.addEventListener('ended', () => {
     flashBtn.classList.remove('pressed');
 });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.min.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+}
